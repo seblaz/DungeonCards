c81127874fa5b445bbb183e32d5faeee3c058568
seblaz <sebastian.blazquez96@gmail.com>
2021-06-25 00:28:46 -0300

    Agrego tests

--- src/main/test/edu/fiuba/algo3/modelo/TableroTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/TableroTest.java
@@ -40,6 +40,8 @@ public class TableroTest {
                 Arguments.of(tablero, cartas[2][0], Tablero.Direccion.DERECHA, cartas[2][1]),
                 Arguments.of(tablero, cartas[2][1], Tablero.Direccion.DERECHA, cartas[2][2]),
                 Arguments.of(tablero, cartas[0][1], Tablero.Direccion.IZQUIERDA, cartas[0][0]),
+                Arguments.of(tablero, cartas[1][2], Tablero.Direccion.IZQUIERDA, cartas[1][1]),
+                Arguments.of(tablero, cartas[2][2], Tablero.Direccion.IZQUIERDA, cartas[2][1]),
                 Arguments.of(tablero, cartas[0][0], Tablero.Direccion.ABAJO, cartas[1][0]),
                 Arguments.of(tablero, cartas[1][0], Tablero.Direccion.ABAJO, cartas[2][0]),
                 Arguments.of(tablero, cartas[1][2], Tablero.Direccion.ABAJO, cartas[2][2])
