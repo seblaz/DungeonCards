167b2ffb4a1bc036f41f3394ee7ad091a68e4413
seblaz <sebastian.blazquez96@gmail.com>
2021-07-09 12:16:28 -0300

    Puedo restar posiciones

--- src/main/java/edu/fiuba/algo3/modelo/Posicion.java
+++ src/main/java/edu/fiuba/algo3/modelo/Posicion.java
@@ -1,5 +1,7 @@
 package edu.fiuba.algo3.modelo;
 
+import java.util.Objects;
+
 public class Posicion {
     private final int x;
     private final int y;
@@ -20,5 +22,22 @@ public class Posicion {
 
     public int y() {
         return this.y;
+    }
+
+    public Posicion restar(Posicion posicion) {
+        return new Posicion(0, 0);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Posicion posicion = (Posicion) o;
+        return x == posicion.x && y == posicion.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
--- src/main/test/edu/fiuba/algo3/modelo/PosicionTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PosicionTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
@@ -37,5 +36,13 @@ public class PosicionTest {
                 Arguments.of(2, 2),
                 Arguments.of(2, 1)
         );
+    }
+
+    @Test
+    public void restarSeASiMismoDevuelveElOrigen() {
+        Posicion posicion = new Posicion(1, 2);
+        Posicion resultado = posicion.restar(posicion);
+        Posicion esperado = new Posicion(0, 0);
+        assertEquals(esperado, resultado);
     }
 }
