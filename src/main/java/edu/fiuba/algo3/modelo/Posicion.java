0e983db028782634874b1ca3f41450075db2c163
seblaz <sebastian.blazquez96@gmail.com>
2021-07-09 12:10:11 -0300

    Se puede pasar la posicion inicial por constructor

--- src/main/java/edu/fiuba/algo3/modelo/Posicion.java
+++ src/main/java/edu/fiuba/algo3/modelo/Posicion.java
@@ -1,11 +1,24 @@
 package edu.fiuba.algo3.modelo;
 
 public class Posicion {
+    private final int x;
+    private final int y;
+
+    public Posicion() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Posicion(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     public int x() {
-        return 0;
+        return this.x;
     }
 
     public int y() {
-        return 0;
+        return this.y;
     }
 }
--- src/main/test/edu/fiuba/algo3/modelo/PosicionTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PosicionTest.java
@@ -2,7 +2,11 @@ package edu.fiuba.algo3.modelo;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,7 +15,27 @@ public class PosicionTest {
     @Test
     public void porDefectoComienzaEnElCeroCero() {
         Posicion posicion = new Posicion();
-        assertEquals(posicion.x(), 0);
-        assertEquals(posicion.y(), 0);
+        assertEquals(0, posicion.x());
+        assertEquals(0, posicion.y());
+    }
+
+    @ParameterizedTest
+    @MethodSource("posicionesIniciales")
+    public void comienzaDondeSeIndiqueEnElConstructor(int x, int y) {
+        Posicion posicion = new Posicion(x, y);
+        assertEquals(x, posicion.x());
+        assertEquals(y, posicion.y());
+    }
+
+    public static Stream<Arguments> posicionesIniciales() {
+        return Stream.of(
+                //           x, y
+                Arguments.of(0, 2),
+                Arguments.of(0, 1),
+                Arguments.of(0, 0),
+                Arguments.of(1, 1),
+                Arguments.of(2, 2),
+                Arguments.of(2, 1)
+        );
     }
 }
